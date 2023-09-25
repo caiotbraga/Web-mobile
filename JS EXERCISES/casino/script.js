4d6019f03a56cbb1cf4f38f86e8f8c919e8de3d0
Caio Braga <caiotabraga@hotmail.com>
2023-09-25 16:51:35 -0300

    Improving drawNumber Script

--- JS EXERCISES/casino/script.js
+++ JS EXERCISES/casino/script.js
@@ -6,10 +6,23 @@ function drawNumber(){
 
     switch (d1) {
         case 1:
-            var img = document.getElementById("dice-1");
-            img.src = ''
+            var img = document.getElementById("dice-1").src = 'dice-1.jpg'
             break;
-    
+        case 2:
+            var img = document.getElementById("dice-1").src = 'dice-2.jpg'
+            break;
+        case 3:
+            var img = document.getElementById("dice-1").src = 'dice-3.jpg'
+            break;
+        case 4:
+            var img = document.getElementById("dice-1").src = 'dice-4.jpg'
+            break;
+        case 5:
+            var img = document.getElementById("dice-1").src = 'dice-5.jpg'
+            break;
+        case 6:
+            var img = document.getElementById("dice-1").src = 'dice-6.jpg'
+            break;
         default:
             break;
     }
