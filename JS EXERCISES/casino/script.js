7c630a3d03c0ca1c528903c02e190f49b56ef454
Caio Braga <108440048+caiotbraga@users.noreply.github.com>
2023-09-26 11:52:57 +0000

    corrigido script

--- JS EXERCISES/casino/script.js
+++ JS EXERCISES/casino/script.js
@@ -4,22 +4,22 @@ function drawNumber1(){
     d1 = getRandomNumber();;
     
     if(d1 === 1){
-        document.getElementById("dice-1").src = "imagens/dice-1.jgp"
+        document.getElementById("dice-1").src = "img/dice-1.jgp"
     }
     else if(d1 === 2){
-        document.getElementById("dice-1").src = "imagens/dice-2.jpg"
+        document.getElementById("dice-1").src = "img/dice-2.jpg"
     }
     else if(d1 === 3){
-        document.getElementById("dice-1").src = "imagens/dice-3.jpg"
+        document.getElementById("dice-1").src = "img/dice-3.jpg"
     }
     else if(d1 === 4){
-        document.getElementById("dice-1").src = "imagens/dice-4.jpg"
+        document.getElementById("dice-1").src = "img/dice-4.jpg"
     }
     else if(d1 === 5){
-        document.getElementById("dice-1").src = "imagens/dice-5.jpg"
+        document.getElementById("dice-1").src = "img/dice-5.jpg"
     }
     else if(d1 === 6){
-        document.getElementById("dice-1").src = "imagens/dice-6.jpg"
+        document.getElementById("dice-1").src = "img/dice-6.jpg"
     }
 
     drawNumber2();
@@ -29,30 +29,28 @@ function drawNumber2(){
     d2 = getRandomNumber();;
     
     if(d2 === 1){
-        document.getElementById("dice-2").src = "imagens/dice-1.jgp"
+        document.getElementById("dice-2").src = "img/dice-1.jpg"
     }
     else if(d2 === 2){
-        document.getElementById("dice-2").src = "imagens/dice-2.jpg"
+        document.getElementById("dice-2").src = "img/dice-2.jpg"
     }
     else if(d2 === 3){
-        document.getElementById("dice-2").src = "imagens/dice-3.jpg"
+        document.getElementById("dice-2").src = "img/dice-3.jpg"
     }
     else if(d2 === 4){
-        document.getElementById("dice-2").src = "imagens/dice-4.jpg"
+        document.getElementById("dice-2").src = "img/dice-4.jpg"
     }
     else if(d2 === 5){
-        document.getElementById("dice-2").src = "imagens/dice-5.jpg"
+        document.getElementById("dice-2").src = "img/dice-5.jpg"
     }
     else if(d2 === 6){
-        document.getElementById("dice-2").src = "imagens/dice-6.jpg"
+        document.getElementById("dice-2").src = "img/dice-6.jpg"
     }
 }
 
 function getRandomNumber() {
 
-    let decimal = Math.random() * 6;
-
-    let IntegerNumber = Math.floor(decimal) + 1;
+    let IntegerNumber = Math.floor(Math.random() * 6) + 1;
 
     return IntegerNumber;
 }
