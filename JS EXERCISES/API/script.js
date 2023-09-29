fe412bf5513a2b424dfadf211517e2752636dedd
Caio Braga <caiotabraga@hotmail.com>
2023-09-29 16:53:23 -0300

    fazendo leitura do elemento cep

--- JS EXERCISES/API/script.js
+++ JS EXERCISES/API/script.js
@@ -1,14 +1,27 @@
 const cep = document.getElementById("cep");
-const adr = document.getElementsByClassName("adress");
-fetchAPI("viacep.com.br/ws/"+cep+"/json/")
+const logradouro = document.getElementById("logradouro");
+const bairro = document.getElementById("bairro");
+const localidade = document.getElementById("localidade");
+const uf = document.getElementById("uf");
 
+var c = cep.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        c = this.value;
+    }
+});
 
 const fetchAPI = (url) =>{
     fetch(url)
     .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        
+        console.log(response)
     })
 }
+
+// const showAdress = (adress) => {
+//     logradouro = document.createTextNode(`Rua : ${adress.lagradouro}`)
+//     bairro = document.createTextNode(`Bairro : ${adress.bairro}`)
+//     localidade = document.createTextNode(`Cidade : ${adress.localidade}`)
+//     uf = document.createTextNode(`Estado : ${adress.uf}`)
+// }
+
+fetchAPI("viacep.com.br/ws/"+c+"/json/")
